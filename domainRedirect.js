d26c8cbe0f7d318cef576b996f211b1d66ab25a1
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-27 21:49:25 -0300

    fixed

--- domainRedirect.js
+++ domainRedirect.js
@@ -1,9 +1,18 @@
 // https://stackoverflow.com/questions/34212039/redirect-to-firebase-hosting-custom-domain
-module.exports = function (domains = ['localhost:5000', 'example.com'], baseurl = 'https://example.com/') {
+module.exports = function (domains = ['localhost:5000', 'example.com'], baseurl = 'https://example.com/', callback) {
     return (req, res, next) => {
+
+        // Invalid Domain Detected
         if (!domains.includes(req.headers['x-forwarded-host'])) {
-            return res.status(301).redirect(url.resolve(baseurl, req.path.replace(/^\/+/, "")));
+
+            if (typeof callback !== "function") {
+                return res.status(301).redirect(baseurl + req.url);
+            }
+
         }
+
+        // Complete
         return next();
+
     };
 };
